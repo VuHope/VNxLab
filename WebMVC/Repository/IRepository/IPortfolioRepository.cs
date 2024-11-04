022810962a7dac28f1c9d915d2b5a0df345ae566
hoang0204 <huyhoang200a@gmail.com>
2024-11-04 18:48:06 +0700

    T1 MAIDINH

---
+++ WebMVC/Repository/IRepository/IPortfolioRepository.cs
@@ -0,0 +1,10 @@
+﻿using Models.Entity;
+
+namespace WebMVC.Repository.IRepository
+{
+    public interface IPortfolioRepository
+    {
+        Task<IEnumerable<Portfolio>> GetAllPortfoliosAsync(string category = null, bool sortByPopularity = false);
+        Task<Portfolio> GetPortfolioByIdAsync(int id);
+    }
+}
