ea6f6048809165e745244d942861dee501a5632b
Vũ Tuấn Anh <tuananh26ttvt@gmail.com>
2024-11-07 22:34:13 +0700

    xoa portfolio

--- WebMVC/Program.cs
+++ WebMVC/Program.cs
@@ -35,7 +35,6 @@ builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductImage, ProductImageRepository>();
-builder.Services.AddScoped<IPortfolioRepository, PortfolioRepository>();
 builder.Services.ConfigureApplicationCookie(options =>
 {
     options.AccessDeniedPath = new PathString("/Account/AccessDenied");
