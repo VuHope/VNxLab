da8a8032242cb88341acbf6254ad89402d197584
LAPTOP-T78D6ENU\nhuhoang <nhuhoang2001524@gmail.com>
2024-11-07 14:16:41 +0700

    Hoangnn - phần Contact - done - 7/11

--- Models/Entity/Contact.cs
+++ Models/Entity/Contact.cs
@@ -8,9 +8,13 @@ namespace Models.Entity
         [Key]
         public int Id { get; set; }
         public string? UserId { get; set; }
+        public string? AdminId { get; set; }
+        public string? Email { get; set; }
         public string? Name { get; set; }
-        public string? Content { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public string? Message { get; set; }
+        public string? Answer { get; set; }
+        public int Status { get; set; } = 0;// 0 - chưa trả lời, 1 - đã trả lời, 2 - từ chối
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
         [ForeignKey("UserId")]
         public virtual ApplicationUser? ApplicationUser { get; set; }
     }
